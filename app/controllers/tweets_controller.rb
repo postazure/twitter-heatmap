01bea90e2745a77c1743ca7e36bcc0dabf48a985
Max Gordon <gordonmaxc@gmail.com>
2015-02-25 10:20:14 -0800

    returning tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,17 @@ class TweetsController < ApplicationController
   def show
     render json:Tweet.find(params[:id])
   end
+
+  def test
+    test = []
+    $twitter.search("#yolo", result_type: "recent").take(3).collect do |tweet|
+      puts "#"*100
+      p tweet.class
+      puts "#"*100
+      test << "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+
+    # @testoutput = "This is a test string from controller"
+    @testoutput = test
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :tweets
+
+  get "/test" => "tweets#test"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
