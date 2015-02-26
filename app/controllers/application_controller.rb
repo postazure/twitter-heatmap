eac7a2b84bb29175b8ec8609f1dccdf332ae7a39
Max Gordon <gordonmaxc@gmail.com>
2015-02-26 00:09:46 -0800

    relocated helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,50 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  def serialize_tweets tweets
+    serialized_tweets = []
+
+    tweets.each do |tweet|
+      tweet_json = {}
+
+      tweet.attributes.each do |attr_name, attr_value|
+        tweet_json[attr_name] = attr_value
+      end
+
+      hashtags = tweet.hashtags
+      hashtag_ids = []
+      hashtags.each do |hashtag|
+        hashtag_ids << hashtag.id
+      end
+      tweet_json["hashtags"] = hashtag_ids
+      
+      serialized_tweets << tweet_json
+    end
+
+    return serialized_tweets
+  end
+
+  def serialize_hashtags hashtags
+    serialized_hashtags = []
+
+    hashtags.each do |hashtag|
+      hashtag_json = {}
+
+      hashtag.attributes.each do |attr_name, attr_value|
+        hashtag_json[attr_name] = attr_value
+      end
+
+      tweets = hashtag.tweets
+      tweet_ids = []
+      tweets.each do |tweet|
+        tweet_ids << tweet.id
+      end
+      hashtag_json["tweets"] = tweet_ids
+      
+      serialized_hashtags << hashtag_json
+    end
+
+    return serialized_hashtags
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -39,7 +39,6 @@ class TweetsController < ApplicationController
   end
 
   private
-
   def extract_hashtags hashtags
     hashtags_text = []
     hashtags.each do |hashtag|
@@ -47,52 +46,4 @@ class TweetsController < ApplicationController
     end
     return hashtags_text
   end
-
-  def serialize_tweets tweets
-    serialized_tweets = []
-
-    tweets.each do |tweet|
-      tweet_json = {}
-
-      tweet.attributes.each do |attr_name, attr_value|
-        tweet_json[attr_name] = attr_value
-      end
-
-      hashtags = tweet.hashtags
-      hashtag_ids = []
-      hashtags.each do |hashtag|
-        hashtag_ids << hashtag.id
-      end
-      tweet_json["hashtags"] = hashtag_ids
-      
-      serialized_tweets << tweet_json
-    end
-
-    return serialized_tweets
-  end
-
-  def serialize_hashtags hashtags
-    serialized_hashtags = []
-
-    hashtags.each do |hashtag|
-      hashtag_json = {}
-
-      hashtag.attributes.each do |attr_name, attr_value|
-        hashtag_json[attr_name] = attr_value
-      end
-
-      tweets = hashtag.tweets
-      tweet_ids = []
-      tweets.each do |tweet|
-        tweet_ids << tweet.id
-      end
-      hashtag_json["tweets"] = tweet_ids
-      
-      serialized_hashtags << hashtag_json
-    end
-
-    return serialized_hashtags
-  end
-
-  
 end
