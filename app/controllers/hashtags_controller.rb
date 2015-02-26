12f3ac7194197f8c3d396624f0ebbef613a7a47f
Max Gordon <gordonmaxc@gmail.com>
2015-02-26 01:40:29 -0800

    serialize results for hashtags

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,5 +1,11 @@
 class HashtagsController < ApplicationController
   def index
-    render json:Hashtag.all
+    tweets = Tweet.all
+    hashtags = Hashtag.all
+
+    serialized_tweets = serialize_tweets(tweets)
+    serialized_hashtags = serialize_hashtags(hashtags)
+
+    render json:{hashtags: serialized_hashtags, tweets: serialized_tweets}
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,37 +13,37 @@ class TweetsController < ApplicationController
     render json:Tweet.find(params[:id])
   end
 
-  def test
-    tweets = []
+  # def test
+  #   tweets = []
     
-    # Search Options
-    lat = 37.7749300
-    lng = -122.4194200
-    radius = 5 #in miles
-    result_count = 10
+  #   # Search Options
+  #   lat = 37.7749300
+  #   lng = -122.4194200
+  #   radius = 5 #in miles
+  #   result_count = 10
 
-    $twitter.search("#",{geocode:"#{lat},#{lng},#{radius}mi"}).take(result_count).collect do |tweet| 
-      hashtags = []
-      hashtags << extract_hashtags(tweet.hashtags) unless tweet.hashtags.empty?
+  #   $twitter.search("#",{geocode:"#{lat},#{lng},#{radius}mi"}).take(result_count).collect do |tweet| 
+  #     hashtags = []
+  #     hashtags << extract_hashtags(tweet.hashtags) unless tweet.hashtags.empty?
 
-      tweets.push({
-        hashtag: hashtags,
-        lat: tweet.geo["coordinates"].first,
-        lng: tweet.geo["coordinates"].last,
-        text: tweet.text,
-        username: tweet.user["screen_name"],
-      })
-    end
+  #     tweets.push({
+  #       hashtag: hashtags,
+  #       lat: tweet.geo["coordinates"].first,
+  #       lng: tweet.geo["coordinates"].last,
+  #       text: tweet.text,
+  #       username: tweet.user["screen_name"],
+  #     })
+  #   end
     
-    render json: tweets
-  end
+  #   render json: tweets
+  # end
 
-  private
-  def extract_hashtags hashtags
-    hashtags_text = []
-    hashtags.each do |hashtag|
-      hashtags_text << hashtag["text"]
-    end
-    return hashtags_text
-  end
+  # private
+  # def extract_hashtags hashtags
+  #   hashtags_text = []
+  #   hashtags.each do |hashtag|
+  #     hashtags_text << hashtag["text"]
+  #   end
+  #   return hashtags_text
+  # end
 end
