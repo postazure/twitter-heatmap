f4287cf97bca9020b6b166bd5aa1982bb3b35e81
Max Gordon <gordonmaxc@gmail.com>
2015-02-26 16:49:14 -0800

    stripped test route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :tweets, :hashtags
-
-  get "/test" => "tweets#test"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
